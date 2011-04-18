d6f381d2089f844106fe25f307d491bcb8f2fccb
Colin Edwards <ddrboxman@gmail.com>
2011-04-18 10:22:09 -0400

    Added upload example using cURL

---
+++ cURL/upload.bat
@@ -0,0 +1,2 @@
+::Note that the editor_id does not have the leading m that is in the page url.
+curl "http://min.us/api/UploadItem?editor_id=dn48vKBiP3q9&key=OK&filename=min.png" -F "file=@min.png" 
